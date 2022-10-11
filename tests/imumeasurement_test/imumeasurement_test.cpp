72334ac3ba7e4f371cf7f87816b3682b7b1787a5
Justin Yu <yujustin8@gmail.com>
2022-10-11 01:40:53 -0400

    working gtest

--- tests/imumeasurement_test/imumeasurement_test.cpp
+++ tests/imumeasurement_test/imumeasurement_test.cpp
@@ -5,8 +5,3 @@ TEST(ImuMeasurementTest, CtorTypeTest) {
   ImuMeasurement<double> imu_data;
   EXPECT_EQ(imu_data.get_type(), 1);
 }
-
-TEST(ImuMeasurementTest, CtorTypeTest2) {
-  ImuMeasurement<double> imu_data;
-  EXPECT_EQ(imu_data.get_type(), 0);
-}
