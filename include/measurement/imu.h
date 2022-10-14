8c4b8fe67cb13b32d396ff07c6470dbe124a8978
Justin Yu <yujustin8@gmail.com>
2022-10-14 00:38:21 -0400

    quaternion test cases

--- include/measurement/imu.h
+++ include/measurement/imu.h
@@ -31,51 +31,56 @@ class ImuMeasurement : public Measurement {
    * @brief Get the 3D rotation matrix for the imu
    * measurement.
    *
-   *
-   * @return Eigen::Matrix3d: 3x3 Matrix.
+   * @return Eigen::Matrix3d: 3x3 Matrix of doubles.
    */
-  Eigen::Matrix3d get_rotation();
+  Eigen::Matrix3d get_rotation_matrix();
 
-  /**
-   * @brief Translate quaternion values to an equivalent rotation matrix.
-   *
-   * @return none
-   */
-  void set_rotation();
-
-  /**
-   * @brief Get the imu measurement quaternion w coefficient.
-   *
-   * @return ImuOrientation<T>: the quaternion w coefficient.
-   */
-  ImuOrientation<T> orientation_w();
 
   /**
    * @brief Get the imu measurement quaternion x coefficient.
    *
    * @return ImuOrientation<T>: the quaternion x coefficient.
    */
-  ImuOrientation<T> orientation_x();
+  T get_quaternion_x();
 
   /**
    * @brief Get the imu measurement quaternion y coefficient.
    *
    * @return ImuOrientation<T>: the quaternion y coefficient.
    */
-  ImuOrientation<T> orientation_y();
+  T get_quaternion_y();
 
   /**
    * @brief Get the imu measurement quaternion z coefficient.
    *
    * @return ImuOrientation<T>: the quaternion z coefficient.
    */
-  ImuOrientation<T> orientation_z();
+  T get_quaternion_z();
+
+  /**
+   * @brief Get the imu measurement quaternion w coefficient.
+   *
+   * @return ImuOrientation<T>: the quaternion w coefficient.
+   */
+  T get_quaternion_w();
+
+  /**
+   * @brief Set the imu measurement quaternion coefficients.
+   *
+   * @param[in] x: x quaternion coefficient.
+   * @param[in] y: y quaternion coefficient.
+   * @param[in] z: z quaternion coefficient.
+   * @param[in] w: w quaternion coefficient.
+   */
+  void set_quaternion(T x, T y, T z, T w);
+
 
  private:
   Eigen::Matrix3d R_;
   ImuOrientation<T> orientation_;
   ImuAngularVelocity<T> angular_velocity_;
   ImuLinearAcceleration<T> linear_acceleration_;
+  void set_rotation();
 };
 //}
 #include "measurement/impl/imu_impl.cpp"
--- tests/imumeasurement_test/imumeasurement_test.cpp
+++ tests/imumeasurement_test/imumeasurement_test.cpp
@@ -1,7 +1,37 @@
 #include <gtest/gtest.h>
 #include "measurement/imu.h"
 
-TEST(ImuMeasurementTest, CtorTypeTest) {
+
+TEST(ImuMeasurementTest, CtorType) {
   ImuMeasurement<double> imu_data;
   EXPECT_EQ(imu_data.get_type(), 1);
+}
+
+TEST(ImuMeasurementTest, QuaternionSetGet) {
+  ImuMeasurement<double> imu_data;
+  imu_data.set_quaternion(0.11, 0.12, 0.22, 0.44);
+  EXPECT_EQ(imu_data.get_quaternion_x(), 0.11);
+  EXPECT_EQ(imu_data.get_quaternion_y(), 0.12);
+  EXPECT_EQ(imu_data.get_quaternion_z(), 0.22);
+  EXPECT_EQ(imu_data.get_quaternion_w(), 0.44);
+}
+
+TEST(ImuMeasurementTest, QuaternionToRotMat) {
+  ImuMeasurement<float> imu_data;
+  float angle = M_PI / 4;
+  float sinA = std::sin(angle / 2);
+  float cosA = std::cos(angle / 2);
+
+  Eigen::Quaternionf q;
+  q.x() = 0 * sinA;
+  q.y() = 1 * sinA;
+  q.z() = 0 * sinA;
+  q.w() = cosA;
+
+  imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w());
+
+  Eigen::Matrix<double, 3, 3> rotmattest;
+  rotmattest << 0.707107, 0, 0.707107, 0, 1, 0, -0.707107, 0, 0.707107;
+
+  EXPECT_NEAR(imu_data.get_rotation_matrix()(0, 0), rotmattest(0, 0), 1e-5);
 }
