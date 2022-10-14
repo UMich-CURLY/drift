7f6affabae84d77945ecfe85285bdd1a250c9dbc
Justin Yu <yujustin8@gmail.com>
2022-10-14 12:23:22 -0400

    style changes

--- tests/imumeasurement_test/imumeasurement_test.cpp
+++ tests/imumeasurement_test/imumeasurement_test.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 #include "measurement/imu.h"
 
-void compare_rotation_matrix(Eigen::Matrix3d imu, Eigen::Matrix3d test);
+void compare_rot_mat(Eigen::Matrix3d imu, Eigen::Matrix3d test);
 
 template<typename T>
-Eigen::Quaternion<T> rotaxis2quaternion(T a, T b_x, T b_y, T b_z);
+Eigen::Quaternion<T> rotaxis2quat(T a, T b_x, T b_y, T b_z);
 
 TEST(ImuMeasurementTest, CtorType) {
   ImuMeasurement<double> imu_data;
@@ -14,8 +14,7 @@ TEST(ImuMeasurementTest, CtorType) {
 TEST(ImuMeasurementTest, QuaternionSetGetBasic) {
   ImuMeasurement<double> imu_data;
 
-  Eigen::Quaterniond q
-      = rotaxis2quaternion<double>(M_PI / 4, M_PI / 2, 0, M_PI / 2);
+  Eigen::Quaterniond q = rotaxis2quat<double>(M_PI / 4, M_PI / 2, 0, M_PI / 2);
   imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w());
 
   // comparison values obtained with
@@ -29,8 +28,7 @@ TEST(ImuMeasurementTest, QuaternionSetGetBasic) {
 TEST(ImuMeasurementTest, QuaternionToRotMat1) {
   ImuMeasurement<float> imu_data;
 
-  Eigen::Quaternionf q
-      = rotaxis2quaternion<float>(M_PI / 4, M_PI / 2, 0, M_PI / 2);
+  Eigen::Quaternionf q = rotaxis2quat<float>(M_PI / 4, M_PI / 2, 0, M_PI / 2);
 
   imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w());
 
@@ -38,34 +36,33 @@ TEST(ImuMeasurementTest, QuaternionToRotMat1) {
   // https://www.andre-gaschler.com/rotationconverter/
   Eigen::Matrix<double, 3, 3> rotmattest;
   rotmattest << 0.707107, 0, 0.707107, 0, 1, 0, -0.707107, 0, 0.707107;
-  compare_rotation_matrix(imu_data.get_rotation_matrix(), rotmattest);
+  compare_rot_mat(imu_data.get_rotation_matrix(), rotmattest);
 }
 
 TEST(ImuMeasurementTest, QuaternionToRotMat2) {
   ImuMeasurement<float> imu_data;
 
-  Eigen::Quaternionf q
-      = rotaxis2quaternion<float>(M_PI / 3, M_PI / 2, M_PI / 2, 0);
+  Eigen::Quaternionf q = rotaxis2quat<float>(M_PI / 3, M_PI / 2, M_PI / 2, 0);
 
   imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w());
 
   Eigen::Matrix<double, 3, 3> rotmattest;
   rotmattest << 0.5, -0.8660254, 0, 0.8660254, 0.5, 0, 0, 0, 1;
-  compare_rotation_matrix(imu_data.get_rotation_matrix(), rotmattest);
+  compare_rot_mat(imu_data.get_rotation_matrix(), rotmattest);
 }
 
 TEST(ImuMeasurementTest, QuaternionToRotMat3) {
   ImuMeasurement<double> imu_data;
 
   Eigen::Quaterniond q
-      = rotaxis2quaternion<double>(M_PI / 6, M_PI / 4, M_PI / 2, M_PI / 4);
+      = rotaxis2quat<double>(M_PI / 6, M_PI / 4, M_PI / 2, M_PI / 4);
 
   imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w());
 
   Eigen::Matrix<double, 3, 3> rotmattest;
   rotmattest << 0.9330127, -0.3535534, 0.0669873, 0.3535534, 0.8660254,
       -0.3535534, 0.0669873, 0.3535534, 0.9330127;
-  compare_rotation_matrix(imu_data.get_rotation_matrix(), rotmattest);
+  compare_rot_mat(imu_data.get_rotation_matrix(), rotmattest);
 }
 
 TEST(ImuMeasurementTest, QuaternionRepresentationInvariant) {
@@ -77,13 +74,13 @@ TEST(ImuMeasurementTest, QuaternionRepresentationInvariant) {
   EXPECT_THROW(imu_data.set_quaternion(0.2, 0.2, 0.2, 0.2),
                std::invalid_argument);
   Eigen::Quaterniond q
-      = rotaxis2quaternion<double>(M_PI / 6, M_PI / 4, M_PI / 2, M_PI / 4);
+      = rotaxis2quat<double>(M_PI / 6, M_PI / 4, M_PI / 2, M_PI / 4);
 
   EXPECT_NO_THROW(imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w()));
 }
 
 
-void compare_rotation_matrix(Eigen::Matrix3d imu, Eigen::Matrix3d test) {
+void compare_rot_mat(Eigen::Matrix3d imu, Eigen::Matrix3d test) {
   for (int r = 0; r < 3; r++) {
     for (int c = 0; c < 3; c++) {
       EXPECT_NEAR(imu(r, c), test(r, c), 1e-5);
@@ -92,16 +89,9 @@ void compare_rotation_matrix(Eigen::Matrix3d imu, Eigen::Matrix3d test) {
 }
 
 
-/**
- * https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Intuition
- *
- * @param[in] a: alpha coefficient.
- * @param[in] b_x: beta x coefficient.
- * @param[in] b_y: beta y coefficient.
- * @param[in] b_z: beta z coefficient.
- */
+// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Intuition
 template<typename T>
-Eigen::Quaternion<T> rotaxis2quaternion(T a, T b_x, T b_y, T b_z) {
+Eigen::Quaternion<T> rotaxis2quat(T a, T b_x, T b_y, T b_z) {
   T sinA = std::sin(a / 2);
   T cosA = std::cos(a / 2);
   Eigen::Quaternion<T> q;
