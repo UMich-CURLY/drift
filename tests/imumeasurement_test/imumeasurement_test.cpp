3ec8b0c3212e9ba97934c69cf2db1f6e7eb2ddb1
Justin Yu <yujustin8@gmail.com>
2022-10-14 12:18:12 -0400

    imu quaternion representation invariant tests

--- tests/imumeasurement_test/imumeasurement_test.cpp
+++ tests/imumeasurement_test/imumeasurement_test.cpp
@@ -18,6 +18,8 @@ TEST(ImuMeasurementTest, QuaternionSetGetBasic) {
       = rotaxis2quaternion<double>(M_PI / 4, M_PI / 2, 0, M_PI / 2);
   imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w());
 
+  // comparison values obtained with
+  // https://www.andre-gaschler.com/rotationconverter/
   EXPECT_NEAR(imu_data.get_quaternion_x(), 0, 1e-5);
   EXPECT_NEAR(imu_data.get_quaternion_y(), 0.3826834, 1e-5);
   EXPECT_NEAR(imu_data.get_quaternion_z(), 0, 1e-5);
@@ -32,7 +34,8 @@ TEST(ImuMeasurementTest, QuaternionToRotMat1) {
 
   imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w());
 
-  // rotmattest created using https://www.andre-gaschler.com/rotationconverter/
+  // rotmattest created using
+  // https://www.andre-gaschler.com/rotationconverter/
   Eigen::Matrix<double, 3, 3> rotmattest;
   rotmattest << 0.707107, 0, 0.707107, 0, 1, 0, -0.707107, 0, 0.707107;
   compare_rotation_matrix(imu_data.get_rotation_matrix(), rotmattest);
@@ -65,6 +68,21 @@ TEST(ImuMeasurementTest, QuaternionToRotMat3) {
   compare_rotation_matrix(imu_data.get_rotation_matrix(), rotmattest);
 }
 
+TEST(ImuMeasurementTest, QuaternionRepresentationInvariant) {
+  ImuMeasurement<double> imu_data;
+
+  EXPECT_THROW(imu_data.set_quaternion(0.123, 0.456, 0.789, 0.012),
+               std::invalid_argument);
+  EXPECT_THROW(imu_data.set_quaternion(1, 2, 3, 4), std::invalid_argument);
+  EXPECT_THROW(imu_data.set_quaternion(0.2, 0.2, 0.2, 0.2),
+               std::invalid_argument);
+  Eigen::Quaterniond q
+      = rotaxis2quaternion<double>(M_PI / 6, M_PI / 4, M_PI / 2, M_PI / 4);
+
+  EXPECT_NO_THROW(imu_data.set_quaternion(q.x(), q.y(), q.z(), q.w()));
+}
+
+
 void compare_rotation_matrix(Eigen::Matrix3d imu, Eigen::Matrix3d test) {
   for (int r = 0; r < 3; r++) {
     for (int c = 0; c < 3; c++) {
@@ -91,5 +109,6 @@ Eigen::Quaternion<T> rotaxis2quaternion(T a, T b_x, T b_y, T b_z) {
   q.y() = std::cos(b_y) * sinA;
   q.z() = std::cos(b_z) * sinA;
   q.w() = cosA;
+
   return q;
 }
