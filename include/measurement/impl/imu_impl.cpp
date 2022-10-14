e7b546c98aec335907b5bdc1e9c0e16edd597bad
Justin Yu <yujustin8@gmail.com>
2022-10-14 00:38:21 -0400

    quaternion test cases

--- include/measurement/impl/imu_impl.cpp
+++ include/measurement/impl/imu_impl.cpp
@@ -5,33 +5,42 @@ ImuMeasurement<T>::ImuMeasurement() {
 }
 
 template<typename T>
-Eigen::Matrix3d ImuMeasurement<T>::toRotationMatrix() {
+Eigen::Matrix3d ImuMeasurement<T>::get_rotation_matrix() {
+  set_rotation();
   return R_;
 }
 
 template<typename T>
 void ImuMeasurement<T>::set_rotation() {
-  Eigen::Quaternion<double> q(orientation_w(), orientation_x(), orientation_y(),
-                              orientation_z());
+  Eigen::Quaternion<double> q(get_quaternion_w(), get_quaternion_x(),
+                              get_quaternion_y(), get_quaternion_z());
   R_ = q.toRotationMatrix();
 }
 
 template<typename T>
-ImuOrientation<T> ImuMeasurement<T>::orientation_w() {
+T ImuMeasurement<T>::get_quaternion_w() {
   return orientation_.w;
 }
 
 template<typename T>
-ImuOrientation<T> ImuMeasurement<T>::orientation_x() {
+T ImuMeasurement<T>::get_quaternion_x() {
   return orientation_.x;
 }
 
 template<typename T>
-ImuOrientation<T> ImuMeasurement<T>::orientation_y() {
+T ImuMeasurement<T>::get_quaternion_y() {
   return orientation_.y;
 }
 
 template<typename T>
-ImuOrientation<T> ImuMeasurement<T>::orientation_z() {
+T ImuMeasurement<T>::get_quaternion_z() {
   return orientation_.z;
+}
+
+template<typename T>
+void ImuMeasurement<T>::set_quaternion(T x, T y, T z, T w) {
+  orientation_.x = x;
+  orientation_.y = y;
+  orientation_.z = z;
+  orientation_.w = w;
 }
