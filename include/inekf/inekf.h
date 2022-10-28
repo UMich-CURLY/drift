26f3d07a2d0ace479bd3167b02ca0241c53932a8
Tingjun-Li <tingjunl@umich.edu>
2022-10-28 14:41:06 -0400

    Updated augmented state

--- include/inekf/inekf.h
+++ include/inekf/inekf.h
@@ -113,50 +113,6 @@ class InEKF {
          */
         NoiseParams get_noise_params() const;
 
-        // ======================================================================
-        /**
-         * @brief Gets the filter's current contact states.
-         * 
-         * @param[in] None
-         * @return std::map<int,bool>: Map of contact ID and bool that indicates if contact is registed
-         */
-        std::map<int,bool> get_contacts() const;
-
-        // ======================================================================
-        /**
-         * @brief Get the current estimated contact positions.
-         * 
-         * @param[in] None
-         * @return std::map<int,int> map of contact ID and associated index in the state matrix X
-         */
-        std::map<int,int> get_estimated_contact_positions() const;
-
-        // ======================================================================
-        /**
-         * @brief Gets the filter's prior landmarks.
-         * 
-         * @param[in] None
-         * @return mapIntVector3d: map of prior landmark ID and position (as a Eigen::Vector3d)
-         */
-        mapIntVector3d get_prior_landmarks() const;
-
-        // ======================================================================
-        /**
-         * @brief Gets the filter's estimated landmarks.
-         * 
-         * @param[in] None
-         * @return std::map<int,int>: map of landmark ID and associated index in the state matrix X
-         */
-        std::map<int,int> get_estimated_landmarks() const;
-
-        // ======================================================================
-        /**
-         * @brief: Gets the filter's set magnetic field.
-         * 
-         * @param[in] None
-         * @return Eigen::Vector3d: magnetic field in world frame
-         */
-        Eigen::Vector3d get_magnetic_field() const;
     /// @}
 
 
@@ -181,22 +137,6 @@ class InEKF {
         void set_noise_params(NoiseParams params);
 
         // ======================================================================
-        /**
-         * @brief Sets the filter's current contact state.
-         * 
-         * @param[in] contacts: A vector of contact ID and indicator pairs. A true indicator means contact is detected.
-         * @return None
-         */
-        void set_contacts(std::vector<ContactState> contacts);
-        
-        // ======================================================================
-        /**
-         * @brief: Sets the filter's prior landmarks.
-         * 
-         * @param[in] prior_landmarks: A map of prior landmark IDs and associated position in the world frame.
-         * @return None
-         */
-        void set_prior_landmarks(const mapIntVector3d& prior_landmarks);
         /** TODO: Sets magnetic field for untested magnetometer measurement */
         void set_magnetic_field(Eigen::Vector3d& true_magnetic_field);
     /// @}
@@ -213,98 +153,8 @@ class InEKF {
          * @return None
          */
         void clear();
-
-        // ======================================================================
-        /**
-         * @brief Removes a single landmark from the filter's prior landmark set.
-         * 
-         * @param[in] landmark_id: The ID for the landmark to remove.
-         * @return None
-         */
-        void RemovePriorLandmarks(const int landmark_id);
-
-        // ======================================================================
-        /**
-         * @brief Removes a set of landmarks from the filter's prior landmark set.
-         * 
-         * @param[in] landmark_ids: A vector of IDs for the landmarks to remove.
-         * @return None
-         */
-        void RemovePriorLandmarks(const std::vector<int> landmark_ids);
-
-        // ======================================================================
-        /**
-         * @brief Removes a single landmark from the filter's estimated landmark set.
-         * 
-         * @param[in] landmark_id: The ID for the landmark to remove.
-         * @return None
-         */
-        void RemoveLandmarks(const int landmark_id);
-
-        // ======================================================================
-        /**
-         * @brief Removes a set of landmarks from the filter's estimated landmark set.
-         * 
-         * @param[in] landmark_ids: A vector of IDs for the landmarks to remove.
-         * @return None
-         */
-        void RemoveLandmarks(const std::vector<int> landmark_ids);
-
-        // ======================================================================
-        /**
-         * @brief Keeps a set of landmarks from the filter's estimated landmark set.
-         * 
-         * @param[in] landmark_ids: A vector of IDs for the landmarks to keep.
-         * @return None
-         */
-        void KeepLandmarks(const std::vector<int> landmark_ids);
     /// @}
 
-
-    /// @name Propagation and Correction Methods
-    /// @{
-        // ======================================================================
-        /**
-         * @brief Propagates the estimated state mean and covariance forward using inertial measurements. 
-         * All landmarks positions are assumed to be static.
-         * All contacts velocities are assumed to be zero + Gaussian noise.
-         * The propagation model currently assumes that the covariance is for the right invariant error.
-         * 
-         * @param[in] imu: 6x1 vector containing stacked angular velocity and linear acceleration measurements
-         * @param[in] dt: double indicating how long to integrate the inertial measurements for
-         * @return None
-         */
-        void Propagate(const Eigen::Matrix<double,6,1>& imu, double dt);
-        
-        // ======================================================================
-        /** 
-         * @brief Corrects the state estimate using the measured forward kinematics between the IMU and a set of contact frames.
-         * If contact is indicated but not included in the state, the state is augmented to include the estimated contact position.
-         * If contact is not indicated but is included in the state, the contact position is marginalized out of the state. 
-         * This is a right-invariant measurement model. Example usage can be found in @include kinematics.cpp
-         * @param[in] measured_kinematics: the measured kinematics containing the contact id, relative pose measurement in the IMU frame, and covariance
-         * @return None
-         */
-        void CorrectKinematics(const vectorKinematics& measured_kinematics); 
-
-        // ======================================================================
-        /** 
-         * @brief Corrects the state estimate using the measured position between a set of contact frames and the IMU.
-         * If the landmark is not included in the state, the state is augmented to include the estimated landmark position. 
-         * This is a right-invariant measurement model.
-         * 
-         * @param[in] measured_landmarks: the measured landmarks containing the contact id, relative position measurement in the IMU frame, and covariance
-         * @return None
-         */
-        void CorrectLandmarks(const vectorLandmarks& measured_landmarks);
-
-        /** TODO: Untested magnetometer measurement*/
-        void CorrectMagnetometer(const Eigen::Vector3d& measured_magnetic_field, const Eigen::Matrix3d& covariance);
-        /** TODO: Untested GPS measurement*/
-        void CorrectPosition(const Eigen::Vector3d& measured_position, const Eigen::Matrix3d& covariance, const Eigen::Vector3d& indices);
-        /** TODO: Untested contact position measurement*/
-        void CorrectContactPosition(const int id, const Eigen::Vector3d& measured_contact_position, const Eigen::Matrix3d& covariance, const Eigen::Vector3d& indices);
-    /// @} 
 
     // Corrects state using invariant observation models
         // ======================================================================
@@ -382,20 +232,16 @@ class InEKF {
      * Testing
      */
 
-    private:
+    protected:
         ErrorType error_type_ = ErrorType::LeftInvariant; 
         bool estimate_bias_ = true;  
         RobotState state_;
         NoiseParams noise_params_;
         const Eigen::Vector3d g_; // Gravity vector in world frame (z-up)
-        std::map<int,bool> contacts_;
-        std::map<int,int> estimated_contact_positions_;
-        mapIntVector3d prior_landmarks_;
-        std::map<int,int> estimated_landmarks_;
-        Eigen::Vector3d magnetic_field_;
+        Eigen::Vector3d magnetic_field_; // Magnetic field vector in world frame (z-up)
 
         Eigen::MatrixXd StateTransitionMatrix(Eigen::Vector3d& w, Eigen::Vector3d& a, double dt);
-        Eigen::MatrixXd DiscreteNoiseMatrix(Eigen::MatrixXd& Phi, double dt);
+        Eigen::MatrixXd DiscreteNoiseMatrix(Eigen::MatrixXd& Phi, double dt, const std::map<int,int>& augmented_states={});
 };
 
 } // end inekf namespace
