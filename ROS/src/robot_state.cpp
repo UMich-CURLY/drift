a24bb59fe3e24f8c64e7234ecc382f99d7a30aaf
Justin Yu <yujustin8@gmail.com>
2022-11-10 22:09:28 -0500

    ros wrapper

---
+++ ROS/src/robot_state.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "robot_state");
+  ros::start();
+
+  if (argc != 3) {
+    cerr << endl
+         << "Usage: rosrun CSE robot_state path_to_vocabulary path_to_settings"
+         << endl;
+    ros::shutdown();
+    return 1;
+  }
+
+  // TODO: Create robot state system -- initialize all system threads
+
+  ros::NodeHandle nodeHandler;
+  ros::Subscriber sub = nodeHandler.subscribe();
+
+  ros::spin();
+
+  // TODO: Stop all threads
+
+  ros::shutdown();
+
+  return 0;
+}
