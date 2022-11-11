4bf838d6bc43b6ef1e0f73fd9f924b4e6f269e08
Justin Yu <yujustin8@gmail.com>
2022-11-11 13:07:45 -0500

    buildros

--- build_ros.sh
+++ build_ros.sh
@@ -3,6 +3,6 @@ echo "Building ROS nodes"
 cd ROS/
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
+cmake .. -DROS_BUILD_TYPE=Release -Wno-dev
 make -j
 # run: export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:PATH/TO/curly_state_estimator/ROS
