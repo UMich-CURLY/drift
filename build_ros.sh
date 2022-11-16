35c2c606dc35524d555d1d462d568fee7d2fe2ba
Justin Yu <yujustin8@gmail.com>
2022-11-16 16:11:28 -0500

    revert to kinematics

--- build_ros.sh
+++
@@ -1,8 +0,0 @@
-echo "Building ROS nodes"
-
-cd ROS/curly_state_estimator
-mkdir build
-cd build
-cmake .. -DROS_BUILD_TYPE=Release -Wno-dev
-make -j
-# run: export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:PATH/TO/curly_state_estimator/ROS/curly_state_estimator
