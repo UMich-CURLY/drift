5a40c0af6c46338ed492746b8185d4087e8f4752
Jonathan Tong <wenzhe.t99@gmail.com>
2022-11-30 02:49:00 -0500

    fix missing SEK3 default constructor issue; add const feature to getters of SEK3

--- src/states/se_k_3.cpp
+++ src/states/se_k_3.cpp
@@ -13,6 +13,9 @@
 using namespace se_k_3;
 
 // constructors
+
+SEK3::SEK3() {}
+
 SEK3::SEK3(const Eigen::MatrixXd& X) : X_(X) {}
 
 SEK3::SEK3(const Eigen::MatrixXd& R, const Eigen::VectorXd& p) {
@@ -28,24 +31,27 @@ SEK3::SEK3(const Eigen::MatrixXd& R, const Eigen::VectorXd& p,
 }
 
 // getters
-Eigen::MatrixXd SEK3::get_X() { return X_; }
-Eigen::MatrixXd SEK3::get_R() { return X_.block(0, 0, 3, 3); }
-Eigen::MatrixXd SEK3::get_p() { return X_.block(0, 3, 3, 1); }
-Eigen::MatrixXd SEK3::get_v() { return X_.block(0, 4, 3, 1); }
-int SEK3::get_dim() { return X_.rows(); }
+const Eigen::MatrixXd SEK3::get_X() const { return X_; }
+const Eigen::MatrixXd SEK3::get_R() const { return X_.block(0, 0, 3, 3); }
+const Eigen::MatrixXd SEK3::get_p() const { return X_.block(0, 3, 3, 1); }
+const Eigen::MatrixXd SEK3::get_v() const { return X_.block(0, 4, 3, 1); }
+const int SEK3::get_K() const { return K_; }
+const int SEK3::get_dim() const { return X_.rows(); }
 
 // setters
 void SEK3::set_X(const Eigen::MatrixXd& X) { X_ = X; }
 void SEK3::set_R(const Eigen::MatrixXd& R) { X_.block(0, 0, 3, 3) = R; }
 void SEK3::set_p(const Eigen::VectorXd& p) { X_.block(0, 3, 3, 1) = p; }
 void SEK3::set_v(const Eigen::MatrixXd& v) { X_.block(0, 4, 3, 1) = v; }
+void SEK3::set_K(int K) { K_ = K; }
 
 // operators
 SEK3 SEK3::operator*(const SEK3& X) {
   SEK3 Y;
   // TODO: check size of X, if not same, throw error
-  Y.set_X(this->X_ * X.X_);
-  Y.set_K(this->K_);
+  Eigen::MatrixXd X1 = X.get_X();
+  Y.set_X(this->get_X() * X1);
+  Y.set_K(this->get_K());
   return Y;
 }
 
@@ -55,6 +61,6 @@ SEK3 SEK3::inverse() {
   Y.set_R(this->get_R().transpose());
   Y.set_p(-this->get_R().transpose() * this->get_p());
   Y.set_v(-this->get_R().transpose() * this->get_v());
-  Y.set_K(this->K_);
+  Y.set_K(this->get_K());
   return Y;
 }
