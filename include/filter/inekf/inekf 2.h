6d76cdfa3f26ba41bf0fa3487fabc748de9024e9
Jonathan-Twz <wenzhe.t99@gmail.com>
2022-12-09 17:54:22 -0500

    merging from dev-jonathan to dev_inekf_and_states

---
+++ include/filter/inekf/inekf 2.h
@@ -0,0 +1,71 @@
+/* ----------------------------------------------------------------------------
+ * Copyright 2022, Tingjun Li, Ross Hartley
+ * All Rights Reserved
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ *  @file   inekf.h
+ *  @author Tingjun Li, Ross Hartley
+ *  @brief  Header file for Invariant EKF.
+ *  Original paper:
+ *  https://journals.sagepub.com/doi/full/10.1177/0278364919894385
+ *  Original github repo:
+ *  https://github.com/RossHartley/invariant-ekf
+ *
+ *  @date   November 25, 2022
+ **/
+
+#ifndef INEKF_INEKF_H
+#define INEKF_INEKF_H
+#include <Eigen/Dense>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+#include "filter/noise_params.h"
+#include "filter/observations.h"
+#include "math/lie_group.h"
+#include "state/robot_state.h"
+
+namespace inekf {
+
+enum ErrorType { LeftInvariant, RightInvariant };
+
+using ContactState = std::pair<int, bool>;
+
+
+/// @}
+// ======================================================================
+/**
+ * @brief Corrects the state using Right Invariant observation model with
+ * given measurement, output and matrices.
+ *
+ * @param[in] Z: innovation matrix
+ * @param[in] H: measurement error matrix
+ * @param[in] N: measurement noise matrix
+ * @param[in/out] state: Robot state
+ * @return None
+ */
+void CorrectRightInvariant(const Eigen::MatrixXd& Z, const Eigen::MatrixXd& H,
+                           const Eigen::MatrixXd& N, RobotState& state,
+                           ErrorType error_type);
+
+// ======================================================================
+/**
+ * @brief Corrects the state using Left Invariant observation model with
+ * given measurement, output and matrices.
+ *
+ * @param[in] Z: innovation matrix
+ * @param[in] H: measurement error matrix
+ * @param[in] N: measurement noise matrix
+ * @param[in/out] state: Robot state
+ * @return None
+ */
+void CorrectLeftInvariant(const Eigen::MatrixXd& Z, const Eigen::MatrixXd& H,
+                          const Eigen::MatrixXd& N, RobotState& state,
+                          ErrorType error_type);
+
+
+}    // namespace inekf
+#endif    // end INEKF_INEKF_H
---
+++ src/state/se_k_3 2.cpp
@@ -0,0 +1,65 @@
+/**
+ *  @file   se_k_3.cpp
+ *  @author Ross Hartley, Wenzhe Tong
+ *  @brief  Source file for various SE(3) functions
+ *  @date   October 5th, 2022
+ **/
+
+#include "state/se_k_3.h"
+
+#include <math.h>
+#include <vector>
+
+
+// constructors
+
+SEK3::SEK3() {}
+
+SEK3::SEK3(const Eigen::MatrixXd& X) : X_(X) {}
+
+SEK3::SEK3(const Eigen::MatrixXd& R, const Eigen::VectorXd& p) {
+  X_.block(0, 0, 3, 3) = R;
+  X_.block(0, 3, 3, 1) = p;
+}
+
+SEK3::SEK3(const Eigen::MatrixXd& R, const Eigen::VectorXd& p,
+           const Eigen::VectorXd& v) {
+  X_.block(0, 0, 3, 3) = R;
+  X_.block(0, 3, 3, 1) = p;
+  X_.block(0, 4, 3, 1) = v;
+}
+
+// getters
+const Eigen::MatrixXd SEK3::get_X() const { return X_; }
+const Eigen::MatrixXd SEK3::get_R() const { return X_.block(0, 0, 3, 3); }
+const Eigen::MatrixXd SEK3::get_p() const { return X_.block(0, 3, 3, 1); }
+const Eigen::MatrixXd SEK3::get_v() const { return X_.block(0, 4, 3, 1); }
+const int SEK3::get_K() const { return K_; }
+const int SEK3::get_dim() const { return X_.rows(); }
+
+// setters
+void SEK3::set_X(const Eigen::MatrixXd& X) { X_ = X; }
+void SEK3::set_R(const Eigen::MatrixXd& R) { X_.block(0, 0, 3, 3) = R; }
+void SEK3::set_p(const Eigen::VectorXd& p) { X_.block(0, 3, 3, 1) = p; }
+void SEK3::set_v(const Eigen::MatrixXd& v) { X_.block(0, 4, 3, 1) = v; }
+void SEK3::set_K(int K) { K_ = K; }
+
+// operators
+SEK3 SEK3::operator*(const SEK3& X) {
+  SEK3 Y;
+  // TODO: check size of X, if not same, throw error
+  Eigen::MatrixXd X1 = X.get_X();
+  Y.set_X(this->get_X() * X1);
+  Y.set_K(this->get_K());
+  return Y;
+}
+
+// methods
+SEK3 SEK3::inverse() {
+  SEK3 Y;
+  Y.set_R(this->get_R().transpose());
+  Y.set_p(-this->get_R().transpose() * this->get_p());
+  Y.set_v(-this->get_R().transpose() * this->get_v());
+  Y.set_K(this->get_K());
+  return Y;
+}
