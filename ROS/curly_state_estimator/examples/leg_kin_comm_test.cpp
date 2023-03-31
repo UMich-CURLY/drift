43e23785ac3ec76a6854fc91e55fdbc5494e79de
李小钧 <tingjunl@umich.edu>
2023-03-31 01:31:47 -0400

    Added tutorial comments to all the examples in ROS/examples

--- ROS/curly_state_estimator/examples/leg_kin_comm_test.cpp
+++ ROS/curly_state_estimator/examples/leg_kin_comm_test.cpp
@@ -22,20 +22,25 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
+  /// TUTORIAL: Initialize ROS node
   ros::init(argc, argv, "robot_state_est");
 
   std::cout << "The subscriber is on!" << std::endl;
 
-  // ros handle handles the start/shutdown for us
+  /// TUTORIAL: Initialize ROS node handle. ROS handle handles the
+  /// start/shutdown for us
   ros::NodeHandle nh;
 
-  // Subscriber:
+  /// TUTORIAL: Create a ROS subscriber
   ros_wrapper::ROSSubscriber ros_sub(&nh);
+
+  /// TUTORIAL: Add a subscriber for IMU data and get its queue and mutex
   std::cout << "Subscribing to imu channel..." << std::endl;
   auto qimu_and_mutex = ros_sub.AddIMUSubscriber("/Imu");
   auto qimu = qimu_and_mutex.first;
   auto qimu_mutex = qimu_and_mutex.second;
 
+  /// TUTORIAL: Add a subscriber for legged kinematics data and get its queue
   std::cout << "Subscribing to joint_states and contact channel..."
             << std::endl;
   auto qkin_and_mutex
@@ -43,16 +48,20 @@ int main(int argc, char** argv) {
   auto qkin = qkin_and_mutex.first;
   auto qkin_mutex = qkin_and_mutex.second;
 
+  /// TUTORIAL: Start the subscriber thread
   ros_sub.StartSubscribingThread();
 
+  /// TUTORIAL: Define some configurations for the state estimator
   inekf::ErrorType error_type = LeftInvariant;
   YAML::Node config_ = YAML::LoadFile(
       "config/filter/inekf/propagation/mini_cheetah_imu_propagation.yaml");
   bool enable_imu_bias_update
       = config_["settings"]["enable_imu_bias_update"].as<bool>();
+
+  /// TUTORIAL: Create a state estimator
   StateEstimator state_estimator(error_type, enable_imu_bias_update);
 
-  // Publisher:
+  /// TUTORIAL: Add a propagation and correction(s) to the state estimator
   // Mini Cheetah's setting:
   state_estimator.add_imu_propagation(
       qimu, qimu_mutex,
@@ -61,16 +70,23 @@ int main(int argc, char** argv) {
       qkin, qkin_mutex,
       "config/filter/inekf/correction/"
       "mini_cheetah_legged_kinematics_correction.yaml");
+
+  /// TUTORIAL: Get the robot state queue and mutex from the state estimator
   RobotStateQueuePtr robot_state_queue_ptr
       = state_estimator.get_robot_state_queue_ptr();
-
   std::shared_ptr<std::mutex> robot_state_queue_mutex_ptr
       = state_estimator.get_robot_state_queue_mutex_ptr();
+
+  /// TUTORIAL: Create a ROS publisher and start the publishing thread
   ros_wrapper::ROSPublisher ros_pub(&nh, robot_state_queue_ptr,
                                     robot_state_queue_mutex_ptr);
   ros_pub.StartPublishingThread();
 
-  // Start running
+  /// TUTORIAL: Run the state estimator. Initialize the bias first, then
+  /// initialize the state. After that, the state estimator will be enabled.
+  /// The state estimator should be run in a loop. Users can use RVIZ to
+  /// visualize the path. The path topic will be something like
+  /// "/robot/*/path"
   while (ros::ok()) {
     // Step behavior
     if (state_estimator.is_enabled()) {
--- ROS/curly_state_estimator/examples/ros_comm_test.cpp
+++ ROS/curly_state_estimator/examples/ros_comm_test.cpp
@@ -22,47 +22,64 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
+  /// TUTORIAL: Initialize ROS node
   ros::init(argc, argv, "robot_state_est");
 
   std::cout << "The subscriber is on!" << std::endl;
 
-  // ros handle handles the start/shutdown for us
+  /// TUTORIAL: Initialize ROS node handle. ROS handle handles the
+  /// start/shutdown for us
   ros::NodeHandle nh;
 
-  // Subscriber:
+  /// TUTORIAL: Create a ROS subscriber
   ros_wrapper::ROSSubscriber ros_sub(&nh);
 
+  /// TUTORIAL: Add a subscriber for IMU data and get its queue and mutex
   auto qimu_and_mutex = ros_sub.AddIMUSubscriber("/gx5_1/imu/data");
   auto qimu = qimu_and_mutex.first;
   auto qimu_mutex = qimu_and_mutex.second;
 
+  /// TUTORIAL: Add a subscriber for velocity data and get its queue and mutex
   auto qv_and_mutex
       = ros_sub.AddDifferentialDriveVelocitySubscriber("/joint_states");
   auto qv = qv_and_mutex.first;
   auto qv_mutex = qv_and_mutex.second;
 
+  /// TUTORIAL: Start the subscriber thread
   ros_sub.StartSubscribingThread();
 
+  /// TUTORIAL: Define some configurations for the state estimator
   inekf::ErrorType error_type = LeftInvariant;
   YAML::Node config_
       = YAML::LoadFile("config/filter/inekf/propagation/imu_propagation.yaml");
   bool enable_imu_bias_update
       = config_["settings"]["enable_imu_bias_update"].as<bool>();
+
+  /// TUTORIAL: Create a state estimator
   StateEstimator state_estimator(error_type, enable_imu_bias_update);
 
-  // Publisher:
-  state_estimator.add_imu_propagation(qimu, qimu_mutex);    // Husky's setting
+  /// TUTORIAL: Add a propagation and correction(s) methods to the state
+  /// estimator. Here is an example of IMU propagation and velocity correction
+  /// for Husky robot
+  state_estimator.add_imu_propagation(qimu, qimu_mutex);
   state_estimator.add_velocity_correction(qv, qv_mutex);
+
+  /// TUTORIAL: Get the robot state queue and mutex from the state estimator
   RobotStateQueuePtr robot_state_queue_ptr
       = state_estimator.get_robot_state_queue_ptr();
-
   std::shared_ptr<std::mutex> robot_state_queue_mutex_ptr
       = state_estimator.get_robot_state_queue_mutex_ptr();
+
+  /// TUTORIAL: Create a ROS publisher and start the publishing thread
   ros_wrapper::ROSPublisher ros_pub(&nh, robot_state_queue_ptr,
                                     robot_state_queue_mutex_ptr);
   ros_pub.StartPublishingThread();
 
-  // Start running
+  /// TUTORIAL: Run the state estimator. Initialize the bias first, then
+  /// initialize the state. After that, the state estimator will be enabled.
+  /// The state estimator should be run in a loop. Users can use RVIZ to
+  /// visualize the path. The path topic will be something like
+  /// "/robot/*/path"
   while (ros::ok()) {
     // Step behavior
     if (state_estimator.is_enabled()) {
