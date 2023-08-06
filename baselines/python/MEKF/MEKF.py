c489ce68c73e1e2f2446f11adfc0274d3f7ce612
Jonathan Tong <wenzhe.t99@gmail.com>
2023-08-06 19:36:39 -0400

    added MEKF baseline and GPS gt, auto bias to be implemented

--- baselines/python/MEKF/MEKF.py
+++ baselines/python/MEKF/MEKF.py
@@ -111,64 +111,64 @@ def read_husky(bag, topic_name):
     return vel_data
     
 
-t0 = time.time()
-# bag = rosbag.Bag('/media/jonathan/SamsungSSD1/tro_data/neya/neya_data/follow_general_2022-12-01-15-44-43_0.bag')
-# bag = rosbag.Bag('/media/justin/DATA/data/husky_data/2022-05-11_MAir/mair_trial1_rectangle.bag')
-# bag = rosbag.Bag('/media/justin/DATA/data/husky_data/2022-05-11_MAir/mair_trial9_M.bag')
-bag = rosbag.Bag('/media/justin/DATA/data/husky_data/northwood/2023-04-24-15-46-01.bag')
+# t0 = time.time()
+# # bag = rosbag.Bag('/media/jonathan/SamsungSSD1/tro_data/neya/neya_data/follow_general_2022-12-01-15-44-43_0.bag')
+# # bag = rosbag.Bag('/media/justin/DATA/data/husky_data/2022-05-11_MAir/mair_trial1_rectangle.bag')
+# # bag = rosbag.Bag('/media/justin/DATA/data/husky_data/2022-05-11_MAir/mair_trial9_M.bag')
+# bag = rosbag.Bag('/media/justin/DATA/data/husky_data/northwood/2023-04-24-15-46-01.bag')
 
-t1 = time.time()
-# imu_data = read_imu(bag, '/mrzr/localization/imu/imu') # neya
-imu_data = read_imu(bag, '/gx5_1/imu/data') # husky
-t_imu = imu_data[:, 0]
-gyro_data = imu_data[:, 1:4]
-acc_data = imu_data[:, 4:7]
-# quat_data = imu_data[:, 7:11]
-# vel_data = read_vel(bag, "/mrzr/localization/wheel_encoder/twist") # neya
-vel_data = read_husky(bag, "/joint_states") # husky
-t_vel = vel_data[:, 0]
-vel_data = vel_data[:, 1:4]
-# R_imu2body = quat2rot([1., 0., 0., 0.]) # neya
-R_imu2body = quat2rot([0, 0.7071068, -0.7071068, 0]) # husky
-# gyro_data = ((gyro_data.T)).T
-# acc_data = ((acc_data.T)).T
-# quat_data = 
+# t1 = time.time()
+# # imu_data = read_imu(bag, '/mrzr/localization/imu/imu') # neya
+# imu_data = read_imu(bag, '/gx5_1/imu/data') # husky
+# t_imu = imu_data[:, 0]
+# gyro_data = imu_data[:, 1:4]
+# acc_data = imu_data[:, 4:7]
+# # quat_data = imu_data[:, 7:11]
+# # vel_data = read_vel(bag, "/mrzr/localization/wheel_encoder/twist") # neya
+# vel_data = read_husky(bag, "/joint_states") # husky
+# t_vel = vel_data[:, 0]
+# vel_data = vel_data[:, 1:4]
+# # R_imu2body = quat2rot([1., 0., 0., 0.]) # neya
+# R_imu2body = quat2rot([0, 0.7071068, -0.7071068, 0]) # husky
+# # gyro_data = ((gyro_data.T)).T
+# # acc_data = ((acc_data.T)).T
+# # quat_data = 
 
-t2 = time.time()
-print("Bag reading time: ", t1-t0)
-print("Process bag time: ", t2-t1)
+# t2 = time.time()
+# print("Bag reading time: ", t1-t0)
+# print("Process bag time: ", t2-t1)
 
-##### neya #####
-# acc_bias = np.array([0.01,   -0.0642287,    -0.155932]).reshape(3)
-# gyro_bias = np.array([-7.49327e-05,  0.000315891,   0.00032303]).reshape(3)
-acc_cov = 0.01 # check in datasheet
-gyro_cov = 0.01 # check in datasheet
-wheel_encoder_cov = 0.034 # check in rosbag
+# ##### neya #####
+# # acc_bias = np.array([0.01,   -0.0642287,    -0.155932]).reshape(3)
+# # gyro_bias = np.array([-7.49327e-05,  0.000315891,   0.00032303]).reshape(3)
+# acc_cov = 0.01 # check in datasheet
+# gyro_cov = 0.01 # check in datasheet
+# wheel_encoder_cov = 0.034 # check in rosbag
 
-##### husky mair #####
-# acc_bias = np.array([0.452427  , -0.0680003,  -0.00248014]) # husky mair
-# gyro_bias = np.array([0.0011257,  0.000182102, -0.000207972]) # husky mair
+# ##### husky mair #####
+# # acc_bias = np.array([0.452427  , -0.0680003,  -0.00248014]) # husky mair
+# # gyro_bias = np.array([0.0011257,  0.000182102, -0.000207972]) # husky mair
 
-acc_bias = np.array([ 0.361971,     0.249837,  -0.00240915]) # husky nw
-gyro_bias = np.array([0.0011879, -2.55878e-05,  -0.00049881  ]) # husky nw
+# acc_bias = np.array([ 0.361971,     0.249837,  -0.00240915]) # husky nw
+# gyro_bias = np.array([0.0011879, -2.55878e-05,  -0.00049881  ]) # husky nw
   
-# acc_cov = 
-# gyro_cov =
-# wheel_encoder_cov = 
+# # acc_cov = 
+# # gyro_cov =
+# # wheel_encoder_cov = 
 
-g = np.array([0, 0, 9.81]).reshape(3)
+# g = np.array([0, 0, 9.81]).reshape(3)
 
-# X = [p, v, q]
-p_est = np.zeros([imu_data.shape[0], 3]) # position estimates
-v_est = np.zeros([imu_data.shape[0], 3]) # velocity estimates
-q_est = np.zeros([imu_data.shape[0], 4]) # quaternion estimates
-p_cov = np.zeros([imu_data.shape[0], 9, 9]) # state covariance
+# # X = [p, v, q]
+# p_est = np.zeros([imu_data.shape[0], 3]) # position estimates
+# v_est = np.zeros([imu_data.shape[0], 3]) # velocity estimates
+# q_est = np.zeros([imu_data.shape[0], 4]) # quaternion estimates
+# p_cov = np.zeros([imu_data.shape[0], 9, 9]) # state covariance
 
-p_est[0] = np.array([0, 0, 0])
-v_est[0] = np.array([0, 0, 0])
-q_est[0] = np.array([1, 0, 0, 0])
-# q_est[0] = np.array([ 0.9990482, 0.0436194, 0, 0 ])
-p_cov[0] = np.zeros([9, 9])
+# p_est[0] = np.array([0, 0, 0])
+# v_est[0] = np.array([0, 0, 0])
+# q_est[0] = np.array([1, 0, 0, 0])
+# # q_est[0] = np.array([ 0.9990482, 0.0436194, 0, 0 ])
+# p_cov[0] = np.zeros([9, 9])
 
 # define measurement update for wheel encoder
 def measurement_update(sensor_var, y, p_cov, p, v, q):
@@ -211,69 +211,67 @@ def measurement_update(sensor_var, y, p_cov, p, v, q):
 ##################################################################
 ################################################################## 
 
-i = 0
-for k in tqdm(range(1, t_imu.shape[0])): # start at 1 because we have initial state
-    # Time difference
+# i = 0
+# for k in tqdm(range(1, t_imu.shape[0])): # start at 1 because we have initial state
+#     # Time difference
 
-    dt = t_imu[k] - t_imu[k-1]
+#     dt = t_imu[k] - t_imu[k-1]
     
-    # 1. Update state with IMU inputs
-    # q_prev = Quaternion(*q_est[k-1, :])
-    # q_curr = Quaternion(axis_angle=(gyro_data[k]-gyro_bias)*dt)
-    # R_ns = q_prev.to_mat()
-    R_ns = quat2rot(q_est[k-1, :])
-    f_ns = (R_ns @ (R_imu2body @ acc_data[k]-acc_bias).reshape((3,1))) - g.reshape((3,1)) # acc_bias in body frame
-    # print(R_ns @ (R_imu2body @ acc_data[k]-acc_bias).reshape((3,1)))
-    # print(f_ns)
-    # print("-------")    
-    assert f_ns.shape == (3,1)
+#     # 1. Update state with IMU inputs
+#     # q_prev = Quaternion(*q_est[k-1, :])
+#     # q_curr = Quaternion(axis_angle=(gyro_data[k]-gyro_bias)*dt)
+#     # R_ns = q_prev.to_mat()
+#     R_ns = quat2rot(q_est[k-1, :])
+#     f_ns = (R_ns @ (R_imu2body @ acc_data[k]-acc_bias).reshape((3,1))) - g.reshape((3,1)) # acc_bias in body frame
+#     # print(R_ns @ (R_imu2body @ acc_data[k]-acc_bias).reshape((3,1)))
+#     # print(f_ns)
+#     # print("-------")    
+#     assert f_ns.shape == (3,1)
     
-    p_ = p_est[k-1, :].reshape((3,1)) + (v_est[k-1, :]*dt).reshape((3,1)) + 0.5*f_ns*dt**2 
-    v_ = v_est[k-1, :].reshape((3,1)) + f_ns*dt
-    # q_ = q_prev.quat_mult_left(q_curr).reshape((4,1))
-    R_predict = R_ns @ expm(skew_symmetric((R_imu2body@gyro_data[k]-gyro_bias)*dt))
-    # print(gyro_data[k])
-    # print("---")
-    q_ = rot2quat(R_predict).reshape((4,1))
+#     p_ = p_est[k-1, :].reshape((3,1)) + (v_est[k-1, :]*dt).reshape((3,1)) + 0.5*f_ns*dt**2 
+#     v_ = v_est[k-1, :].reshape((3,1)) + f_ns*dt
+#     # q_ = q_prev.quat_mult_left(q_curr).reshape((4,1))
+#     R_predict = R_ns @ expm(skew_symmetric((R_imu2body@gyro_data[k]-gyro_bias)*dt))
+#     # print(gyro_data[k])
+#     # print("---")
+#     q_ = rot2quat(R_predict).reshape((4,1))
     
-    # 1.1 get motion model Jacobian
-    # F = np.eye(9)
-    # F[0:3, 3:6] = np.eye(3)*dt
-    # F[3:6, 6:9] = -R_ns @ skew_symmetric((acc_data[k]-acc_bias))*dt
-    # F[6:9, 6:9] = Quaternion(aq_xis_angle=(gyro_data[k]-gyro_bias)*dt).to_mat().T
+#     # 1.1 get motion model Jacobian
+#     # F = np.eye(9)
+#     # F[0:3, 3:6] = np.eye(3)*dt
+#     # F[3:6, 6:9] = -R_ns @ skew_symmetric((acc_data[k]-acc_bias))*dt
+#     # F[6:9, 6:9] = Quaternion(aq_xis_angle=(gyro_data[k]-gyro_bias)*dt).to_mat().T
     
-    zero = np.zeros((3,3))
-    I = np.eye(3)
-    F = expm( np.block([[ zero ,   I,                                            zero],
-                        [ zero ,   zero, -R_predict @ skew_symmetric(R_imu2body@acc_data[k] - acc_bias)],
-                        [ zero ,   zero,      -skew_symmetric(R_imu2body@gyro_data[k]-gyro_bias)]])*dt )
+#     zero = np.zeros((3,3))
+#     I = np.eye(3)
+#     F = expm( np.block([[ zero ,   I,                                            zero],
+#                         [ zero ,   zero, -R_predict @ skew_symmetric(R_imu2body@acc_data[k] - acc_bias)],
+#                         [ zero ,   zero,      -skew_symmetric(R_imu2body@gyro_data[k]-gyro_bias)]])*dt )
 
-    # 1.2 uncertainty propagation
-    q_cov = np.zeros([9, 9]) # IMU noise covariance
-    q_cov[0:3, 0:3] = 0.000001*np.eye(3)
-    q_cov[3:6, 3:6] = acc_cov * np.eye(3) *dt**2
-    q_cov[6:9, 6:9] = gyro_cov * np.eye(3) *dt**2
+#     # 1.2 uncertainty propagation
+#     q_cov = np.zeros([9, 9]) # IMU noise covariance
+#     q_cov[0:3, 0:3] = 0.000001*np.eye(3)
+#     q_cov[3:6, 3:6] = acc_cov * np.eye(3) *dt**2
+#     q_cov[6:9, 6:9] = gyro_cov * np.eye(3) *dt**2
     
-    l_jac = np.zeros((9,6))
-    l_jac[3:, :] = np.eye(6) # motion model noise jacobian
-    p_cov_ = F @ p_cov[k-1, :, :] @ F.T + q_cov 
+#     l_jac = np.zeros((9,6))
+#     l_jac[3:, :] = np.eye(6) # motion model noise jacobian
+#     p_cov_ = F @ p_cov[k-1, :, :] @ F.T + q_cov 
     
-    # # 1.3 velocity update
-    # if(k<t_vel.shape[0] and abs(t_imu[k]-t_vel[i]) <= abs(t_imu[k+1]-t_vel[i]) and i<vel_data.shape[0]):
-    #     p_, v_, q_, p_cov_ = measurement_update(wheel_encoder_cov, vel_data[i, :].reshape((3,1)), p_cov_, p_, v_, q_)
-    #     i+=1
-    if i<vel_data.shape[0] and t_imu[k] >= t_vel[i]:
-        p_, v_, q_, p_cov_ = measurement_update(wheel_encoder_cov, vel_data[i, :].reshape((3,1)), p_cov_, p_, v_, q_)
-        i+=1
-        
+#     # # 1.3 velocity update
+#     # if(k<t_vel.shape[0] and abs(t_imu[k]-t_vel[i]) <= abs(t_imu[k+1]-t_vel[i]) and i<vel_data.shape[0]):
+#     #     p_, v_, q_, p_cov_ = measurement_update(wheel_encoder_cov, vel_data[i, :].reshape((3,1)), p_cov_, p_, v_, q_)
+#     #     i+=1
+#     if i<vel_data.shape[0] and t_imu[k] >= t_vel[i]:
+#         p_, v_, q_, p_cov_ = measurement_update(wheel_encoder_cov, vel_data[i, :].reshape((3,1)), p_cov_, p_, v_, q_)
+#         i+=1
     
-    # 1.4 update states
-    p_est[k, :] = p_.T
-    v_est[k, :] = v_.T
-    q_est[k, :] = q_.T
-    p_cov[k, :, :] = p_cov_.T
+#     # 1.4 update states
+#     p_est[k, :] = p_.T
+#     v_est[k, :] = v_.T
+#     q_est[k, :] = q_.T
+#     p_cov[k, :, :] = p_cov_.T
 
-# print(i)   
     
 def save_pose_to_tum(t, p, q, filename='tum_traj.txt'):
     # t = data[:, 0]
@@ -284,8 +282,10 @@ def save_pose_to_tum(t, p, q, filename='tum_traj.txt'):
     # qx = np.zeros(t.shape[0])
     # qy = np.zeros(t.shape[0])
     # qz = np.zeros(t.shape[0])
-    s = 10
-    t = t
+    s = 1
+    if t.shape[0] > 10000:
+        s = 10
+    t = t[::s]
     x = p[::s, 0]
     y = p[::s, 1]
     z = p[::s, 2]
@@ -293,13 +293,12 @@ def save_pose_to_tum(t, p, q, filename='tum_traj.txt'):
     qx = q[::s, 1]
     qy = q[::s, 2]
     qz = q[::s, 3]
-    # print(t.shape, x.shape, y.shape, z.shape, qw.shape, qx.shape, qy.shape, qz.shape)
     tum_data = np.stack((t, x, y, z, qx, qy, qz, qw), axis=1)
 
     # save to txt file
     np.savetxt(filename, tum_data, delimiter=' ')
     
-save_pose_to_tum(t_imu, p_est, q_est, filename='tum_estimated.txt')
+# save_pose_to_tum(t_imu, p_est, q_est, filename='tum_estimated.txt')
 #### 6. Results and Analysis ###################################################################
 
 ################################################################################################
